96f3abd4ef77e0f5bd05705df35fd1ae334a2871
bb010g <me@bb010g.com>
2020-09-08 21:27:33 -0700

    Slightly cleaner jq in buildscripts

--- buildscripts/docker/scripts/psp_scripts/build_pspdev.sh
+++ buildscripts/docker/scripts/psp_scripts/build_pspdev.sh
@@ -9,9 +9,7 @@ WORKDIR="$(pwd)"
 cd "${script_root}"
 
 latest_release_url="$(curl -L "https://api.github.com/repos/pspdev/pspdev/releases?per_page=100" \
-    | jq -r '[.[] | select(.prerelease | not)][].assets[].browser_download_url' \
-    | grep 'linux-x64' \
-    | head -n 1)"
+    | jq -r 'first(.[] | select(.prerelease | not).assets[].browser_download_url | contains("linux-x64"))')"
 
 echo "$(basename "$(dirname "${latest_release_url}")")" > "${WORKDIR}/pspdev_rev.txt"
 
--- buildscripts/docker/scripts/vita_scripts/build_vita-shader-collection.sh
+++ buildscripts/docker/scripts/vita_scripts/build_vita-shader-collection.sh
@@ -9,9 +9,7 @@ WORKDIR="$(pwd)"
 cd "${script_root}"
 
 latest_release_url="$(curl -L "https://api.github.com/repos/frangarcj/vita-shader-collection/releases?per_page=100" \
-    | jq -r '[.[] | select(.prerelease | not)][].assets[].browser_download_url' \
-    | grep 'master' \
-    | head -n 1)"
+    | jq -r 'first(.[] | select(.prerelease | not).assets[].browser_download_url | contains("master"))')"
 
 echo "$(basename "$(dirname "${latest_release_url}")")" > "${WORKDIR}/vita-shader-collection_rev.txt"
 
