838e64dcb2083284dbf597771abdc89965d6722f
Andy Nguyen <theofficialflow1996@gmail.com>
2020-09-23 19:10:24 +0200

    Hotfix

--- cef/systemctrl/main.c
+++ cef/systemctrl/main.c
@@ -328,8 +328,8 @@ void OnSystemStatusIdle() {
 
 	// Set fake framebuffer so that cwcheat can be displayed
 	if (adrenaline->pops_mode) {
-		sceDisplaySetFrameBuf661((void *)0x0A000000, PSP_SCREEN_LINE, PSP_DISPLAY_PIXEL_FORMAT_8888, PSP_DISPLAY_SETBUF_NEXTFRAME);
-		memset((void *)0xAA000000, 0, SCE_PSPEMU_FRAMEBUFFER_SIZE);
+		sceDisplaySetFrameBuf661((void *)NATIVE_FRAMEBUFFER, PSP_SCREEN_LINE, PSP_DISPLAY_PIXEL_FORMAT_8888, PSP_DISPLAY_SETBUF_NEXTFRAME);
+		memset((void *)NATIVE_FRAMEBUFFER, 0, SCE_PSPEMU_FRAMEBUFFER_SIZE);
 	} else {
 		SendAdrenalineCmd(ADRENALINE_VITA_CMD_RESUME_POPS);
 	}
